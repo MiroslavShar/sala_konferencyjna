bcce2cbbe56ca111bc5a307acd51e2aeda7e2c7c
Mirosław Jewtuszenko <mje@MiroslawJewtuszenkoMac.local>
2023-12-16 17:05:01 +0100

    Add new hall

--- app_sala_konferencyjna/models.py
+++ app_sala_konferencyjna/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Hall(models.Model):
+    name_hall = models.CharField(max_length=255)
+    capacity_hall = models.IntegerField
+    availability_of_projector = models.BooleanField(default=True)
--- app_sala_konferencyjna/views.py
+++ app_sala_konferencyjna/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+
+from app_sala_konferencyjna.models import Hall
+
+def add_new_hall(request):
+    if request.method == 'GET':
+        response = render(request, 'add_room.html')
+        return response
+    else:
+        name_hall = request.POST['name_hall']
+        capacuty_hall = request.POST['capacity_hall']
+        h = Hall(name_hall=name_hall, capacity_hall=capacuty_hall)
+        h.save()
+        return render(request,'new_room.html', context={'hall':h})
+
 
 # Create your views here.
